e2f61e47b908f30d7a04725895b9fb30180f1615
DecemberHB <27hyobin@gmail.com>
2025-10-24 11:51:47 +0900

    Flutter 10/24 #1

--- ch01/10_generic_collections.dart
+++ ch01/10_generic_collections.dart
@@ -1,5 +1,139 @@
 /*
-  날짜 : 2025/10/23
+  날짜 : 2025/10/24
   이름 : 박효빈
-  내용 : Dart 함수
+  내용 : Dart 제네릭 컬랙션
 */
+
+class Box<T> {
+  T item;
+
+  Box(this.item);
+
+  T getItem() {
+    return item;
+  }
+}
+void main() {
+
+  // 제네릭 객체 생성
+  Box stringBox = Box<String>('hello');
+ String text = stringBox.getItem();
+ print('text : $text');
+
+ Box intBox = Box<int>(100);
+ int num = intBox.getItem();
+ print('num : $num');
+
+ // 컬렉션 (자료구조)
+ //////////////////////////////////////////////////////
+ /////////// LIST
+ //////////////////////////////////////////////////////
+ // List 생성
+  List<String> fruits = ['Apple','Banana','Cherry'];
+  print('fruits : $fruits');
+
+  // 요소 추가
+  fruits.add('Durian');
+  print('fruits : $fruits');
+
+  // 요소 참조
+  print('첫번쨰 과일 : ${fruits[0]}');
+  print('첫번쨰 과일 : ${fruits.first}');
+  print('마지막 과일 : ${fruits.last}');
+
+  // 원소 수정
+  fruits[1] = 'Berry';
+  print('fruits : $fruits');
+
+  // 원소 제거
+  String removedFruits = fruits.removeAt(0);
+  print('removed : $removedFruits');
+  print('fruits : $fruits');
+
+  // 리스트 크기
+  print('과일 갯수 : ${fruits.length}');
+
+  // 리스트 반복문
+  for (String fruit in fruits) {
+    print('과일 : $fruit');
+  }
+
+  // 리스트 필터링
+  List<int> numbers = [1,2,3,4,5];
+  var evenNumbers = numbers.where((n) => n % 2 == 0);
+  print('짝수 : $evenNumbers');
+
+  // 변환 (많이 사용함) 리스트 개별 요소들에 대한 처리 (map) < 람다식
+  var doubledList = numbers.map((n) => n * 2).toList();
+  print('doubleList : $doubledList');
+
+
+  // 정렬
+  numbers.sort();
+  print('오름차순 : $numbers');
+
+  numbers.sort((a,b) => b.compareTo(a));
+  print('내림차순 : $numbers');
+
+  // 반복 처리
+  numbers.forEach((n) => print('n값 : $n'));
+
+
+  //////////////////////////////////////////////////////
+  /////////// Set, 중복 제거 자료구조
+  //////////////////////////////////////////////////////
+
+  Set<String> colors = {'red','green','blue'};
+  print('colors : $colors');
+
+  // 원소 추가
+  colors.add('orange');
+  colors.add('red');
+  print('colors : $colors'); //red는 중복 제거되서 안 들어감
+
+  // 집합 연산(합집합)
+  var set1 = {1,2,3,4};
+  var set2 = {3,4,5,6};
+
+  Set<int> unionSet = set1.union(set2);
+  print('합집합 : $unionSet');
+
+  // 교집합
+  Set<int> intersectSet = set1.intersection(set2);
+  print('교집합 : $intersectSet'); // 3,4
+
+  // 차집합
+  Set<int> differnceSet = set1.difference(set2);
+  print('차집합 : $differnceSet'); // A U B에서 교집합 외 남은 A 부분
+
+
+
+
+  //////////////////////////////////////////////////////
+  /////////// Map (중요) List <> Map
+  //////////////////////////////////////////////////////
+
+  // 맵 생성
+  Map<String, String> user = {
+    'id' : 'a101',
+    'name' : '고양이',
+    'address' : '캣타워'
+  };
+
+  print('user : $user');
+
+  // Map 키 참조
+  print('이름 : ${user['name']}');
+  print('주소 : ${user['address']}');
+
+  // 키 존재 여부
+  print('age 키 존재 여부 : ${user.containsKey('age')}');
+
+  // 모든 키, 값 들 리스트 생성
+  print('모든 키 목록 : ${user.keys.toList()}');
+  print('모든 값 목록 : ${user.values.toList()}');
+
+
+}
+
+
--- ch01/11_future_async_await.dart
+++ ch01/11_future_async_await.dart
@@ -1,5 +1,94 @@
 /*
-  날짜 : 2025/10/23
+  날짜 : 2025/10/24
   이름 : 박효빈
-  내용 : Dart 함수
+  내용 : Dart 비동기 처리 * 중요 *
 */
+// 함수 정의
+// Future는 미래에 가져오는 값을 의미하는 객체 (비동기)
+import 'dart:async';
+
+Future<String> fetchData(){
+  // 서버에서 데이터를 가져오는 백엔드 작업 (비동기 작업 정의) - 시간 걸리는 작업을 딜레이로 표현 한 것뿐 deleye >> 통신 들어갈 예정
+  return Future.delayed(Duration(seconds: 3),(){ // 3초 진행
+    return 'fetchData!';
+  });
+}
+
+// Stream을 반환하는 함수
+Stream<int> countStream() async*{
+
+  for(int i=1; i<=5; i++){
+    await Future.delayed(Duration(seconds :1));
+    yield i; // 스트림으로 데이터sms 내보내기(emit)
+  }
+
+}
+
+void main() async {
+
+  // 비동기 작업 수행
+  print('here...1');
+  fetchData()
+      .then((data){
+        // 비동기 함수 결과값
+
+        print('here...2 : $data');
+  })
+      .catchError((err){
+        // 비동기 함수에서 에러가 났을떄
+        print('here...3 : $err');
+  })
+      .whenComplete((){
+        // 작업 종료
+        print('here...4 비동기 작업 완료!');
+  });
+
+    print('here...5'); // 결과값 1 - 5 - 2 - 4
+
+  // async/await 실행
+  print('async_await...1');
+
+
+  try {
+    String data = await fetchData(); // await 가 fetch를 대기 >> async main 뒤에 붙혀줌
+    print('async_await...2 : $data');
+  }catch(e) {
+    print('async_await...3 : $e');
+  }finally {
+    print('async_await...4 비동기 작업 완료');
+  }
+
+  print('async_await...5'); // 순서대로 1 - 2 - 4 - 5
+
+
+  /*
+  * Future vs Stream
+  *  - Future는 한 번만 비동기 값을 반환하는 비동기 처리
+  *  - Stream은 시간이 지남에 따라 여러 데이터를 순차적으로 전달하는 비동기 처리 (잘 안씀 - 채팅, 센서 장비에 씀)
+  * */
+  // Stream 작업
+  final controller = StreamController<String>(); // String은 스트림으로 보내는 데이터 타입
+
+  // Stream 리스너 등록
+  controller.stream
+      .listen((data){
+        print('stream...1 : $data');
+  })
+      .onError((err){
+        print('stream error');
+  });
+
+  // 스트림 데이터 전송
+  controller.sink.add('hello'); //$data << 여기에
+  controller.sink.add('welcome');
+  controller.sink.add('greeting');
+
+  // Stream 함수 호출 후 await for 출력
+  Stream<int> stream = countStream();
+
+  await for(int number in stream){
+    print('스트림 데이터 수신 : $number');
+
+  }
+
+}
